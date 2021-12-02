26c4efe9d737964ea96fa3863ebf65e7a9576b8c
aliosmanakca <akcaaliosman@gmail.com>
2021-12-02 11:45:10 -0500

    .

---
+++ src/Questions/Q1.java
@@ -0,0 +1,13 @@
+package Questions;
+
+public class Q1 {
+    /*1---Sayının Sayı değerlerinin toplamasını yapan Java kodu yazınız.
+
+    Test Data
+    34
+    Beklenen Çıktı
+    7*/
+    public static void main(String[] args) {
+
+    }
+}
