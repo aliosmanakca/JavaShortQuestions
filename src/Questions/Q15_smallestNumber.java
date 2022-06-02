ff03dd3fce35cee7523b04485df43fdedce0d45a
aliosmanakca <akcaaliosman@gmail.com>
2022-06-02 13:02:37 -0400

    .

--- src/Questions/Q15_smallestNumber.java
+++ src/Questions/Q15_smallestNumber.java
@@ -43,12 +43,10 @@ public class Q15_smallestNumber {
     private static void smallestNumberSecondWay(int[] arr) {
         int smallest = arr[0];
 
-        for (int i = 0; i<arr.length; i++) {
-            for (int j = i+1; j<arr.length; j++) {
-                if (arr[i] > arr[j] && arr[j] <= smallest) {
-                    smallest = arr[j];
+        for (int i = 0; i<arr.length-1; i++) {
+                if (arr[i] > arr[i+1] && arr[i+1] <= smallest) {
+                    smallest = arr[i+1];
                 }
-            }
         }
 
         System.out.println(smallest);
