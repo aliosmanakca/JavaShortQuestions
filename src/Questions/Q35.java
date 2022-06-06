bf30c5006210703461c4870473791062ef317470
aliosmanakca <akcaaliosman@gmail.com>
2022-06-06 13:23:38 -0400

    .

---
+++ src/Questions/Q35.java
@@ -0,0 +1,46 @@
+package Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q35 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int number = scan.nextInt();
+        scan.nextLine();
+        List<String> list = new ArrayList<>();
+        String str = "";
+
+        for (int i=0; i<number; i++){
+            str=scan.nextLine();
+            list.add(str);
+        }
+
+        for (int i=0; i<number; i++){
+            String listTemp="";
+
+            String arr[] = list.get(i).split(" ");
+            String newArr[] = new String[arr.length];
+
+            newArr[0]=arr[0];
+            newArr[arr.length-1]=arr[arr.length-1];
+
+            for (int j=1; j<arr.length-1; j++){
+                newArr[j]=arr[(arr.length-1)-j];
+            }
+
+            for (int k=0; k<newArr.length; k++){
+                listTemp += newArr[k] + " ";
+            }
+            list.set(i,listTemp);
+        }
+
+        for (int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+
+    }
+}
